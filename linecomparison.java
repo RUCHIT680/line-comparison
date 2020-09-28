357c0d37ccbabd937a457597fc9aed98bd4737fe
RUCHIT680 <ruchitrai282@gmail.com>
2020-09-28 16:42:07 +0530

    added UC5

--- linecomparison.java
+++ linecomparison.java
@@ -1,19 +1,20 @@
 
-public class linecomparison { System.out.println("length1 " +x1 +"," +y1+ " and "+x2 +"," +y2 + " is " +length1 );
-System.out.println("length2 " +x3 +"," +y3+ " and "+x4 +"," +y4 + " is " +length2 );
-
-Double l1 = Double.valueOf(length1);
+public class linecomparison { Double l1 = Double.valueOf(length1);
 Double l2 = Double.valueOf(length2);
 
 boolean compareValue = l1.equals(l2);
+int m = l1.compareTo(l2);
 
 if (compareValue)
+if (m == 0)
 	System.out.println("Both lines are equal");
 else
 	System.out.println("Both lines are not equal");
 }
+}
 
 }
+
 
 
 
