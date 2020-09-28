bb67abaef77ebad440164767b4c1f48beba5f0fe
RUCHIT680 <ruchitrai282@gmail.com>
2020-09-28 16:39:13 +0530

    added UC4

--- linecomparison.java
+++ linecomparison.java
@@ -1,13 +1,16 @@
 
-public class linecomparison { public static void main(String[] args) {int y3 = (int) (Math.floor(Math.random()*10)) ;
-int x4 = (int) (Math.floor(Math.random()*10)) ;
-int y4 = (int) (Math.floor(Math.random()*10)) ;
-
-length1 = (int)  Math.sqrt(Math.pow(x2-x1, 2)+ Math.pow(y2-y1, 2));
-length2 = (int)  Math.sqrt(Math.pow(x4-x3, 2)+ Math.pow(y4-y3, 2));
-
-System.out.println("length1 " +x1 +"," +y1+ " and "+x2 +"," +y2 + " is " +length1 );
+public class linecomparison { System.out.println("length1 " +x1 +"," +y1+ " and "+x2 +"," +y2 + " is " +length1 );
 System.out.println("length2 " +x3 +"," +y3+ " and "+x4 +"," +y4 + " is " +length2 );
+
+Double l1 = Double.valueOf(length1);
+Double l2 = Double.valueOf(length2);
+
+boolean compareValue = l1.equals(l2);
+
+if (compareValue)
+	System.out.println("Both lines are equal");
+else
+	System.out.println("Both lines are not equal");
 }
 
 }
