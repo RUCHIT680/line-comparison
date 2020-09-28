8384deafc9fecf5537c15d1334828f900b763019
RUCHIT680 <ruchitrai282@gmail.com>
2020-09-28 16:06:50 +0530

    commit

---
+++ linecomparison.java
@@ -0,0 +1,18 @@
+
+public class linecomparison { public static void main(String[] args) {
+	System.out.println("Welcome to Line Comparison Program");
+	double length1 = 0 , length2 = 0 ;
+	int x1 = (int) (Math.floor(Math.random()*10)) ;
+	int y1 = (int) (Math.floor(Math.random()*10)) ;
+	int x2 = (int) (Math.floor(Math.random()*10)) ;
+	int y2 = (int) (Math.floor(Math.random()*10)) ;
+	int x3 = (int) (Math.floor(Math.random()*10)) ;
+	int y3 = (int) (Math.floor(Math.random()*10)) ;
+	int x4 = (int) (Math.floor(Math.random()*10)) ;
+	int y4 = (int) (Math.floor(Math.random()*10)) ;
+}
+
+}
+
+
+
